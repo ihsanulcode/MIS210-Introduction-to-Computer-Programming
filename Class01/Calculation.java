a7821b3242e28165ab8848b226c6c54451940af8
Ihsanul Haque Asif <66785935+ihsanulcode@users.noreply.github.com>
2022-06-08 18:15:59 +0600

    Class01-Java Setup & Syntax

---
+++ Class01/Calculation.java
@@ -0,0 +1,21 @@
+
+public class Calculation {
+	public static void main(String[] args) {
+		//sum of two numbers;
+		int a = 2;
+		int b = 3;
+		
+		int sum = a*b; //a+b = 2+3 = 5
+		
+		//System.out.println("Summation: "+sum);
+		//sum of 2 and 3 is 5
+		System.out.println("Multiplication of "+a+" and "+b+" is "+sum);
+		
+		//Operators
+		//sum +
+		//sub -
+		//multiplication *
+		//division /
+		//reminder %
+	}
+}
---
+++ Class01/Task.java
@@ -0,0 +1,12 @@
+//Print you name, Id, and Department
+public class Task { //starting scope
+	//psvm
+	public static void main(String[] args) {
+		
+		//printing my name, id and department
+		System.out.println("Ihsanul Haque");
+		System.out.println("201");
+		System.out.println("ECE Major CSE"); //to-down theory
+	}
+
+} //ending scope
